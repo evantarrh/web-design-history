682364c030278f2cfdb3adc41a2372b5f8e61b3d
Evan Tarrh <evantarrh@gmail.com>
2014-10-18 22:45:32 -0400

    populating the database with the websites in plain text

---
+++ main.py
@@ -0,0 +1,16 @@
+from backend import databaseMethods as db
+
+from flask import Flask, render_template, redirect, url_for, session
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+@app.route('/')
+def hello():
+	db.fillSnapshotDatabase()
+	#db.snapshots.find()
+
+	return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
